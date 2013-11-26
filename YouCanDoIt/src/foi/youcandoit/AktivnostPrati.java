519e70f7f337380cda91287a7a322f159670ffc6
mdzeko <mdzeko@hotmail.com>
2013-11-26 11:34:41 +0100

    biblioteka google play services

--- YouCanDoIt/src/foi/youcandoit/AktivnostPrati.java
+++ YouCanDoIt/src/foi/youcandoit/AktivnostPrati.java
@@ -1,20 +1,29 @@
 package foi.youcandoit;
 
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
+
+import foi.youcandoit.sucelja.ILokacija;
+
 import android.app.Activity;
+import android.content.Context;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.maps.model.LatLng;
+
 public class AktivnostPrati extends Activity
 {
 	public int time = 0;
 	public boolean zaust = false;
+	private LatLng trenutnaLokacija;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -22,6 +31,20 @@ public class AktivnostPrati extends Activity
 		setContentView(R.layout.aktivnost_prati);
 		Button pokreni = (Button) findViewById(R.id.gmbPokreni);
 		Button zaustavi = (Button) findViewById(R.id.gmbZaustavi);
+		//EditText lok = (EditText) findViewById(R.id.urediDuljina);
+		
+		LocationManager mlocManager=null;  
+        ILokacija mlocListener;  
+        mlocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);  
+        mlocListener = new ILokacija();  
+       mlocManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
+       
+       if (mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) 
+       {
+            Log.d("Latitude:- ", "" + ILokacija.duzina + '\n');  
+            Log.d("Longitude:- ", "" + ILokacija.sirina + '\n');  
+       }
+		
 		pokreni.setOnClickListener(new OnClickListener() {
 			
 			@Override
